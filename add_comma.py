0214526934b45b89ab33dc57608741d0ce7bfc5c
aokenu <34852856+aokenu@users.noreply.github.com>
2025-12-22 18:52:29 +0100

    Updated project files

--- add_comma.py
+++ add_comma.py
@@ -1,1 +1,11 @@
+import os
+from unittest import result 
+import csv
+import datetime
+import time
+import mysql.consnector
+import threading
+from config import mysql_conn, mysql_connv
+#from mysql.connector import Error 
+
 

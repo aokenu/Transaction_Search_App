ebfafe01b1c66e7aa727665815f9a3d3566d550a
aokenu <34852856+aokenu@users.noreply.github.com>
2025-12-22 18:53:09 +0100

    Updated project files

--- add_comma.py
+++ add_comma.py
@@ -9,3 +9,10 @@ from config import mysql_conn, mysql_connv
 #from mysql.connector import Error 
 
 
+import smtplib,ssl
+from os.path import basename
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.application import MIMEApplication
+
+

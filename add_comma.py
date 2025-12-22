4920c498cffa95b7938426d9bdeedbdca72bcfd8
aokenu <34852856+aokenu@users.noreply.github.com>
2025-12-22 18:57:19 +0100

    Updated project files

--- add_comma.py
+++ add_comma.py
@@ -51,4 +51,6 @@ with open(path1, 'r') as file_object:
         #print(lines)
      session_ids=lines.split()
     #print(f"' + {banks} + '", sep="','" )
+    print(session_ids)
+    fieldnames=['BVN','first_name','Middle_name','Surname','DOB','Account','Bank']
     
